27b7840e5515b117e8f8ce2b39756cfb75194efe
Maulanaamir <achmadmaulanaamirudin@gmail.com>
2025-06-28 15:08:33 +0700

    update

--- frontend/src/layouts/AdminLayout.jsx
+++ frontend/src/layouts/AdminLayout.jsx
@@ -4,9 +4,14 @@ import Sidebar from "../components/Sidebar";
 
 export default function AdminLayout() {
   return (
-    <div className="flex">
-      <Sidebar />
-      <main className="flex-1 bg-gray-100 p-6 min-h-screen">
+    <div className="flex min-h-screen">
+      {/* Sticky Sidebar */}
+      <aside className="sticky top-0 h-screen">
+        <Sidebar />
+      </aside>
+
+      {/* Main Content */}
+      <main className="flex-1 bg-gray-100 p-6 overflow-y-auto">
         <Outlet />
       </main>
     </div>
